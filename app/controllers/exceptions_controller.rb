17d205fd21f3a2cb8f099e6994ecc3785bc787d9
Batou99 <lorenzo.lopez@farncombe.com>
2014-05-29 14:32:44 +0100

    Basic print of exceptions

--- app/controllers/exceptions_controller.rb
+++ app/controllers/exceptions_controller.rb
@@ -1,3 +1,5 @@
+require 'digest/sha1'
+
 class ExceptionsController < ApplicationController
   before_filter :load_target
 
@@ -16,6 +18,18 @@ class ExceptionsController < ApplicationController
   end
 
   def index
-    @exs = @target.all.to_a
+    from = params[:from] || 10.years.ago
+    to = params[:to] || Time.now
+    @title = 'Exceptions'
+    @exs = @target.where(:updated_at.gt => from).where(:updated_at.lt => to).to_a
+    @summary = {}
+    @exs.each { |ex|
+      prm = eval(ex.params)
+      key = "#{prm["controller"]}-#{prm["action"]}" 
+      key = ex.class_name if key == "-"
+      key = Digest::SHA1.hexdigest(key)[0..10]
+      @summary[key] = [] if !@summary[key]
+      @summary[key] << ex
+    }
   end
 end
